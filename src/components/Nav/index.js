25665925c1e106591b2e890c07c270bc7b685535
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-29 01:53:09 -0400

    csd

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import CollapsibleContact from '../CollapsibleContact';
+import { M } from 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import { RiCodeSSlashLine, RiAccountCircleFill, RiHome2Fill } from 'react-icons/ri';
 import './style.css';
@@ -8,7 +9,6 @@ import './style.css';
 class Sidenav extends Component {
 
   componentDidMount() {
-    const M = window.M
     let sidenav = M.Sidenav.init(this.Sidenav, {draggable: true,});
     sidenav.close();
   }
