93387acb75015b3e15b941596bd974e20714eff5
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-29 00:22:46 -0400

    sf

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,10 +10,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
