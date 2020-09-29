99383e8064424960e2aa0107057c89305b950df3
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-29 03:25:27 -0400

    successful serve build

---
+++ build/precache-manifest.4fa34fd61212063a8b01bbb84caec65b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed854638971aa4e180d978fb24fdb61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25aa990259e46ac1b231",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "3e5bab9d69f10fadd9b7",
+    "url": "/static/css/main.017181fb.chunk.css"
+  },
+  {
+    "revision": "25aa990259e46ac1b231",
+    "url": "/static/js/2.5d357df3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d357df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5bab9d69f10fadd9b7",
+    "url": "/static/js/main.ab7336b3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "f6297aa60295aa4df493677d4551a97c",
+    "url": "/static/media/background.f6297aa6.jpg"
+  },
+  {
+    "revision": "2c2cce7d8d8fddd670dfdffba4cbccad",
+    "url": "/static/media/bindr.2c2cce7d.png"
+  },
+  {
+    "revision": "cadb9199102c6f1ea27b8905cd1f6caf",
+    "url": "/static/media/eat-da-burger.cadb9199.png"
+  },
+  {
+    "revision": "750a5512ee042b5fb229706ad05a39ce",
+    "url": "/static/media/fitness-tracker.750a5512.png"
+  },
+  {
+    "revision": "101e4b53f26f80cdf3610512c0289939",
+    "url": "/static/media/movie-finder.101e4b53.png"
+  },
+  {
+    "revision": "f66b833fa5ae3eae84cd0dfb6ec369d1",
+    "url": "/static/media/parallax-img.f66b833f.jpg"
+  },
+  {
+    "revision": "39346e92cf282c16ea549ec56df264cb",
+    "url": "/static/media/profile.39346e92.jpg"
+  },
+  {
+    "revision": "c33f5840e01e78a326e4099287bc9c69",
+    "url": "/static/media/react-recipes.c33f5840.png"
+  },
+  {
+    "revision": "5b34fa3f3ab1e4884cc8db20d3128b7a",
+    "url": "/static/media/sidenav.5b34fa3f.jpg"
+  },
+  {
+    "revision": "fe025c6b347f78bb439a0d2d71908dc0",
+    "url": "/static/media/weather-dash.fe025c6b.png"
+  }
+]);
