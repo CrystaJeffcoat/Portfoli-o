1f8aeff8509b03f6a0c2116d615d65d3eef362a3
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-29 02:50:03 -0400

    fd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css';
-import About from "./pages/About";
 import Projects from "./pages/Projects";
-import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Route path='/' component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/projects' component={Projects} />
-    </Router>
+    <Projects />
   )
 }
 
